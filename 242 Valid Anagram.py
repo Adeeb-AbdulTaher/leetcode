6b8917b2c653b5aaea897f230d01ac81cc9d2c45
Adeeb-AbdulTaher <54802363+Adeeb-AbdulTaher@users.noreply.github.com>
2024-06-11 12:02:37 -0500

    Create 242 Valid Anagram.py

---
+++ 242 Valid Anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
+
+#my initial code
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ss=list(s)
+        tt=list(t)
+        if len(ss)!=len(tt):
+            return False
+        for i in ss:
+            if i not in tt:
+                return False
+            else:
+                tt.remove(i)
+        return True
