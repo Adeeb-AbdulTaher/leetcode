6efd71dc38126955dbc11fd2c2e1dc74d0fd6365
Adeeb-AbdulTaher <54802363+Adeeb-AbdulTaher@users.noreply.github.com>
2024-06-24 13:11:33 -0500

    Update 125 Valid Palindrome.py

--- 125 Valid Palindrome.py
+++ 125 Valid Palindrome.py
@@ -1,3 +1,12 @@
+
+#2 liner code
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=re.sub(r'[\W_]+', '', s).lower()
+        #s = [i for i in s.lower() if i.isalnum()] has bette time, slightly
+        return s == s[::-1]
+
+
 # my initial solution
 class Solution:
     def isPalindrome(self, s: str) -> bool:
