f54031fd78ac6362c4d3083f4f3070a3adce751c
Adeeb-AbdulTaher <54802363+Adeeb-AbdulTaher@users.noreply.github.com>
2024-07-01 13:20:48 -0500

    Update 167 Two Sum II - Input Array Is Sorted.py

--- 167 Two Sum II - Input Array Is Sorted.py
+++ 167 Two Sum II - Input Array Is Sorted.py
@@ -1,3 +1,22 @@
+# as an exercise, tried the binary search solution too
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        investigated = []
+        for i in range(len(numbers)):
+            if not numbers[i] in investigated:
+                investigated.append(numbers[i])
+                l, r = i + 1, len(numbers) - 1
+                tmp = target - numbers[i]
+                while l <= r:
+                    mid = l + (r-l) // 2
+                    if numbers[mid] == tmp:
+                        return([i + 1, mid + 1])
+                    elif numbers[mid] < tmp:
+                        l = mid + 1
+                    else:
+                        r = mid - 1
+
 
 # used dic for this like two sum problem, BUT used enumerate here and it was way better
 class Solution:
@@ -9,7 +28,7 @@ class Solution:
             diffs[num]=i
 
 
-#my first simple logic solution. worst 5%. most did under 100ms, this took 5000ms [O(n)]
+#my first simple logic solution. worst 5%. most did under 100ms, this took 5000ms [O(n^2)]
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         diff=[target-i for i in numbers]
